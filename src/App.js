ec4fb5bcccc24a74b2cd4fc4e876bc5625aae789
TechnoGecko <cr5697@gmail.com>
2022-01-30 02:50:27 -0800

    Home header finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import "./index.css";
+import About from "./Pages/About/About.js";
+import Contact from "./Pages/Contact/Contact.js";
+import Home from "./Pages/Home/Home.js";
+import Services from "./Pages/ServicesPage/Services.js";
+import { Routes, Route, Link } from "react-router-dom";
+import Navbar from "./Components/Navbar/Navbar";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="rmnw__navbar-holder">
+        <Navbar />
+      </div>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/services" element={<Services />} />
+        <Route exact path="/about" element={<About />} />
+        <Route exact path="/contact" element={<Contact />} />
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
